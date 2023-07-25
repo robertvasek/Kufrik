6b1482bf07de988e76660675b6b1b035f9203ec2
Robert Vašek <xvasek1@fi.muni.cz>
2023-07-26 00:47:18 +0200

    Initial playing and settings

--- travel_co/Controllers/HomeController.cs
+++ travel_co/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Zájezdy()
+    {
+        return View();
+    }
+
+    public IActionResult Průvodce()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
