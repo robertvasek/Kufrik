2b7d7b3571cb28edec0bd35690e856cb14902b7e
robertvasek <robert.vashek@gmail.com>
2023-08-09 15:19:34 +0200

    delete old model

--- travel_co/Models/Post.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Post
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Subtitle { get; set; }
-    public string Content { get; set; }
-    public string[] ImageUrls { get; set; }
-}
-
