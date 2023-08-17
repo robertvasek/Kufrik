f5079b15598e3175bc5556b40be947cd684ce7f4
robertvasek <robert.vashek@gmail.com>
2023-08-17 12:41:42 +0200

    remade appearing of header, only appears on scroll up, not on bottom and top of page

--- travel_co/wwwroot/js/site.js
+++ travel_co/wwwroot/js/site.js
@@ -1,1 +1,26 @@
-﻿// empty
+﻿let lastScrollPosition = 0;
+const header = document.getElementById('fixed-header');
+let isAtBottom = false;
+
+window.addEventListener('scroll', () => {
+    const currentScrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    if (currentScrollPosition + windowHeight >= documentHeight || currentScrollPosition <= 0) {
+        isAtBottom = true;
+    } else {
+        isAtBottom = false;
+    }
+
+    if (!isAtBottom) {
+        if (currentScrollPosition < lastScrollPosition) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    }
+
+    lastScrollPosition = currentScrollPosition;
+});
+
