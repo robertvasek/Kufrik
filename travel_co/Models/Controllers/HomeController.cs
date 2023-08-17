3ce6a95b7dd7e382320e40be552af27fdd58bfcd
robertvasek <robert.vashek@gmail.com>
2023-08-17 14:41:06 +0200

    fixed position of question boxes, appearing nice even on phones

--- travel_co/Models/Controllers/HomeController.cs
+++ travel_co/Models/Controllers/HomeController.cs
@@ -33,8 +33,7 @@ public class HomeController : Controller
 
     private List<Question> Questions = new List<Question>
     {
-        new Question { QuestionQ = "Jak zjistím veškeré podrobné info o daném zájezdu?", AnswerQ = "Veškeré podrobné info je dostupné v .pdf souboru, který je dostupný po zaplacení první zálohy." +
-            " Předcházíme tak odcizení plánu výletu." },
+        new Question { QuestionQ = "Jak zjistím veškeré podrobné info o daném zájezdu?", AnswerQ = "Veškeré podrobné info je dostupné v .pdf souboru, který je dostupný po zaplacení první zálohy." },
         new Question { QuestionQ = "Do kdy můžu zrušit svou rezervaci na zájezd?", AnswerQ = "Rezervaci je možné bezplatně zrušit do zaplacení první zálohy, která bývá zpravidla dva měsíce před začátkem zájezdu." },
         new Question { QuestionQ = "Je potřebné si koupit pojištění na zájezd?", AnswerQ = "Ne, není. Pojištění zajišťujeme hromadně pro všechny účastníky zájezdu a je tak součástí ceny zájezdu." },
     };
