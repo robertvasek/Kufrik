3eb153204867f545fccf32f06d484487427c4c5f
robertvasek <robert.vashek@gmail.com>
2023-08-28 18:15:15 +0200

    made complaints and added new controller just for info, now complaint rules are done and div is kinda done

---
+++ travel_co/Models/Controllers/InfoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace travel_co.Models.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult InformationCentre()
+        {
+            return View();
+        }
+
+        public IActionResult Complaints()
+        {
+            return View();
+        }
+    }
+}
+
