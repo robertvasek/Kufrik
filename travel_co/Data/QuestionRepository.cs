4f8534b5b08deb695d2d7e237c5ef75f89b42ff3
robertvasek <robert.vashek@gmail.com>
2023-08-28 17:07:38 +0200

    added few questions, trying to edit footer and add information centre

--- travel_co/Data/QuestionRepository.cs
+++ travel_co/Data/QuestionRepository.cs
@@ -8,6 +8,9 @@ namespace travel_co.Data
         new Question { QuestionQ = "Jak zjistím veškeré podrobné info o daném zájezdu?", AnswerQ = "Veškeré podrobné info je dostupné v .pdf souboru, který je dostupný po zaplacení první zálohy." },
         new Question { QuestionQ = "Do kdy můžu zrušit svou rezervaci na zájezd?", AnswerQ = "Rezervaci je možné bezplatně zrušit do zaplacení první zálohy, která bývá zpravidla dva měsíce před začátkem zájezdu." },
         new Question { QuestionQ = "Je potřebné si koupit pojištění na zájezd?", AnswerQ = "Ne, není. Pojištění zajišťujeme hromadně pro všechny účastníky zájezdu a je tak součástí ceny zájezdu." },
+        new Question { QuestionQ = "Od, popř. do jakého věku je možné s námi cestovat?", AnswerQ = "Věková hranice není určena, avšak každý účastník by měl zvážit, zda-li je jeho zdravotní stav v dostatečně dobrém stavu" +
+            ", popř. zda-li je věk dítěte úměrný náročnosti zájezdu."},
+        new Question { QuestionQ = "Dle čeho se vyhodnocují reklamace na případné stížnosti nebo potíže, které nastaly během zájezdu?", AnswerQ = "Vše se vyhodnocuje dle reklamačního řádu, viz. konec stránky."}
     };
     }
 }
